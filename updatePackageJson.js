0c8eb8eec69cc2e4b01348e03db6566ee45c0371
Tu Phan <pt27122000@gmail.com>
2024-07-07 16:23:00 +0700

    chore: update lib info

---
+++ updatePackageJson.js
@@ -0,0 +1,13 @@
+const editJsonFile = require("edit-json-file");
+
+// If the file doesn't exist, the content will be an empty object by default.
+const sourceFile = editJsonFile(`${__dirname}/package.json`);
+const targerFile = editJsonFile(`${__dirname}/dist/ngx-lazy-directive/package.json`);
+
+targerFile
+    .set('repository', sourceFile.get('repository'))
+    .set('author', sourceFile.get('author'))
+    .set('keywords', sourceFile.get('keywords'))
+    .set('homepage', sourceFile.get('homepage'));
+
+targerFile.save();
